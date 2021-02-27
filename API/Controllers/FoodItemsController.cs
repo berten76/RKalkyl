c4f280d3aede48f18754afabe4763898ff34cc2c
Christofer Berten <christofer.berten@se.abb.com>
2021-02-27 20:38:00 +0100

    clean

--- API/Controllers/FoodItemsController.cs
+++ API/Controllers/FoodItemsController.cs
@@ -36,7 +36,7 @@ namespace API.Controllers
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteFoodItem(Guid id)
-        {
+        { 
             return Ok(await Mediator.Send(new Delete.Command(){Id = id}));
         }
     }
