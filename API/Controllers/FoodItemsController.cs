3bf9677409501d8a9f0969847fdf92171cfc3bb9
Christofer Berten <christofer.berten@se.abb.com>
2021-02-27 19:59:19 +0100

    Add crud operation to API

--- API/Controllers/FoodItemsController.cs
+++ API/Controllers/FoodItemsController.cs
@@ -34,7 +34,7 @@ namespace API.Controllers
             return Ok( await Mediator.Send(new Edit.Command(){FoodItem = foodItem}));
         }
 
-        [HttpDelete("{id")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteFoodItem(Guid id)
         {
             return Ok(await Mediator.Send(new Delete.Command(){Id = id}));
